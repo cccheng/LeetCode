94d951546bbb96bb1cbb554e174bc322fdbbc0dd
Chung-Chiang Cheng <cccheng@synology.com>
2020-06-27 22:55:32 +0800

    0001. Two Sum (rust)
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

---
+++ 0001-two_sum.rs
@@ -0,0 +1,24 @@
+
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut ans = Vec::new();
+        for i in 0..nums.len() {
+            for j in (i+1)..nums.len() {
+                if nums[i] + nums[j] == target {
+                    ans.push(i as i32);
+                    ans.push(j as i32);
+                    return ans;
+                }
+            }
+        }
+        unreachable!()
+    }
+}
+
+fn main()
+{
+    assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+    assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+}
