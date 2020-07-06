c78fc0690611cb7f4eac289c81445e9302a571dc
Chung-Chiang Cheng <cccheng@synology.com>
2020-07-06 15:35:53 +0800

    change directory layout for rust
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- rust/src/s0001_two_sum.rs
+++ rust/src/s0001_two_sum.rs
@@ -1,7 +1,9 @@
 
+#[allow(dead_code)]
 struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut ans = Vec::new();
         for i in 0..nums.len() {
@@ -17,7 +19,8 @@ impl Solution {
     }
 }
 
-fn main()
+#[test]
+fn test_two_sum()
 {
     assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
     assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
