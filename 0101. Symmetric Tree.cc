4846b5745a9b64134b513b88629fcd1a14d6d4c8
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-01-25 22:38:31 +0800

    101. Symmetric Tree
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

---
+++ 0101. Symmetric Tree.cc
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isSymmetricTree(TreeNode* left, TreeNode* right) {
+        if (!left || !right)
+            return left == right;
+        else if (left->val != right->val)
+            return false;
+        else
+            return isSymmetricTree(left->left, right->right) &&
+                   isSymmetricTree(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL)
+            return true;
+        else
+            return isSymmetricTree(root->left, root->right);
+    }
+};
