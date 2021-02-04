6fd02493da9dd40ee5c40f34c21483366f113b79
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-02-04 20:31:37 +0800

    141. Linked List Cycle
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

---
+++ 0141. Linked List Cycle.cc
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *p = head;
+        ListNode *q = head;
+        
+        while (q) {
+            p = p->next;
+            
+            q = q->next;
+            if (!q) return false;
+            q = q->next;
+            if (!q) return false;
+            if (p == q)
+                return true;
+        }
+        return false;
+    }
+};
