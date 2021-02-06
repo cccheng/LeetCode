8a7d714fea44287cdc0c25259a1bd413dd48c71c
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-02-06 11:44:50 +0800

    94. Binary Tree Inorder Traversal. v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0094. Binary Tree Inorder Traversal.cc
+++ 0094. Binary Tree Inorder Traversal.cc
@@ -24,3 +24,43 @@ public:
         return output;
     }
 };
+
+// v2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> stack;
+        TreeNode* cur = root;
+
+        while (cur) {
+            stack.push(cur);
+            cur = cur->left;
+        }
+
+        while (!stack.empty()) {
+            cur = stack.top();
+            result.push_back(cur->val);
+            stack.pop();
+
+            cur = cur->right;
+            while (cur) {
+                stack.push(cur);
+                cur = cur->left;
+            }
+        }
+
+        return result;
+    }
+};
