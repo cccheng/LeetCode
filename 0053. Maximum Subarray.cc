ef2bfb42060f8cf0be9578797d21466b3d57c31b
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-02-16 01:56:58 +0800

    53. Maximum Subarray.cc
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0053. Maximum Subarray.cc
+++ 0053. Maximum Subarray.cc
@@ -1,9 +1,10 @@
+// v1 Kadane's algorithm
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int cur_sum = nums[0];
         int result = cur_sum;
-        
+
         for (size_t i = 1; i < nums.size(); ++i) {
             if (cur_sum + nums[i] < nums[i])
                 cur_sum = nums[i];
@@ -11,6 +12,24 @@ public:
                 cur_sum = cur_sum + nums[i];
             result = max(cur_sum, result);
         }
+        return result;
+    }
+};
+
+// v2 DP
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> table(nums.size());
+
+        table[0] = nums[0];
+        int result = table[0];
+
+        for (int i = 1; i < table.size(); ++i) {
+            table[i] = nums[i] + (table[i-1] < 0 ? 0 : table[i-1]);
+            result = max(result, table[i]);
+        }
+
         return result;
     }
 };
