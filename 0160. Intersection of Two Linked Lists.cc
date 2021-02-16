933335229242db218434c8aa85dd8a71c1040e03
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-02-16 12:00:19 +0800

    160. Intersection of Two Linked Lists
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0160. Intersection of Two Linked Lists.cc
+++ 0160. Intersection of Two Linked Lists.cc
@@ -25,3 +25,44 @@ public:
         return nullptr;
     }
 };
+
+// v2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0, lenB = 0;
+
+        if (!headA || !headB)
+            return nullptr;
+
+        for (auto p = headA; p; p = p->next)
+            lenA++;
+        for (auto p = headB; p; p = p->next)
+            lenB++;
+
+        if (lenA > lenB) {
+            for (; lenA > lenB; headA = headA->next, lenA--)
+                ;
+        } else {
+            for (; lenB > lenA; headB = headB->next, lenB--)
+                ;
+        }
+
+        while (headA && headB) {
+            if (headA == headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+
+        return nullptr;
+    }
+};
