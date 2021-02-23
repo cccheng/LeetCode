b27ee07e970ca461ce4b29123175d6d1f06ed67c
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-02-23 16:24:51 +0800

    0240 v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0240. Search a 2D Matrix II.cc
+++ 0240. Search a 2D Matrix II.cc
@@ -16,3 +16,31 @@ public:
         return false;
     }
 };
+
+// v2
+class Solution {
+public:
+    bool _search(vector<vector<int>>& m, int v, int rbegin, int rend, int cbegin, int cend) {
+        if (rbegin + 1 >= rend && cbegin + 1 >= cend)
+            return m[rbegin][cbegin] == v;
+
+        int rmid = rbegin + (rend - rbegin) / 2;
+        int cmid = cbegin + (cend - cbegin) / 2;
+
+        if (m[rmid][cmid] == v)
+            return true;
+        else if (m[rmid][cmid] > v) {
+            return _search(m, v, rbegin, rmid, cbegin, cmid) ||
+                _search(m, v, rmid, rend, cbegin, cmid) ||
+                _search(m, v, rbegin, rmid, cmid, cend);
+        } else {
+            return _search(m, v, rmid, rend, cmid, cend) ||
+                _search(m, v, rmid, rend, cbegin, cmid) ||
+                _search(m, v, rbegin, rmid, cmid, cend);
+        }
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        return _search(matrix, target, 0, matrix.size(), 0, matrix.front().size());
+    }
+};
