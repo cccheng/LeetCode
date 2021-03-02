8ffbd168854d9c594e864834cd02ea40bf3288c9
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-02 17:01:43 +0800

    0078 v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0078. Subsets.cc
+++ 0078. Subsets.cc
@@ -3,13 +3,13 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<bool> visited(nums.size());
         vector<vector<int>> result;
-        
+
         result.push_back({});
-        
+
         function<void(int,vector<int>)> bt = [&](int len, vector<int> cur) {
             if (len > nums.size())
                 return;
-            
+
             cur.resize(len);
             for (int i = nums.size() - 1; i >= 0; --i) {
                 if (visited[i])
@@ -21,9 +21,33 @@ public:
                 visited[i] = false;
             }
         };
-        
+
         bt(1, {});
-        
+
+        return result;
+    }
+};
+
+// v2
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+
+        function<void(int,vector<int>)> bt = [&](int offset, vector<int> cur) {
+            result.push_back(cur);
+
+            if (offset >= nums.size())
+                return;
+
+            for (int i = offset; i < nums.size(); ++i) {
+                cur.push_back(nums[i]);
+                bt(i + 1, cur);
+                cur.pop_back();
+            }
+        };
+
+        bt(0, {});
         return result;
     }
 };
