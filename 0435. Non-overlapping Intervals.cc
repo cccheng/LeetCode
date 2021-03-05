9367807e9d49e82e4efa0c98e5cdefd206b0252e
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-05 23:06:11 +0800

    435. Non-overlapping Intervals
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0435. Non-overlapping Intervals.cc
+++ 0435. Non-overlapping Intervals.cc
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& ints) {
-        if (ints.size() <= 1)
-            return 0;
-
-        sort(ints.begin(), ints.end(),
-             [](const auto& a, const auto& b) {
-                 return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
-             }
-            );
-
         int count = 0;
-        vector<vector<int>> result;
-        result.push_back(ints[0]);
-        for (int i = 1; i < ints.size(); ++i) {
-            if (result.back()[1] > ints[i][0]) {
-                count++;
-                result.back()[1] = min(result.back()[1], ints[i][1]);
-            } else {
-                result.push_back(ints[i]);
+
+        sort(ints.begin(), ints.end(), [](auto& a, auto&b) {
+            return a[1] < b[1];
+        });
+
+        for (auto i = 0; i < ints.size(); ) {
+            int j = i + 1;
+            while (j < ints.size() && ints[j][0] < ints[i][1]) {
+                j++; count++;
             }
+            i = j;
         }
+
         return count;
     }
 };
