e7367b27ba3ae545c23f85742a8b80c96e49eb4e
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-07 23:40:14 +0800

    0787. v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0787. Cheapest Flights Within K Stops.cc
+++ 0787. Cheapest Flights Within K Stops.cc
@@ -2,42 +2,35 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
         vector<vector<int>> edge(n+1, vector<int>(n+1, -1));
-        for (auto f : flights)
+        for (auto& f : flights)
             edge[f[0]][f[1]] = f[2];
-        
+
         typedef tuple<int,int,int> Node; // stop, node, current price
         auto cmp = [](auto& a, auto& b) -> bool {
                 return get<2>(a) > get<2>(b);
             }; // min heap
         priority_queue<Node, vector<Node>, decltype(cmp)> pq(cmp);
-        vector<vector<int>> price(K+1, vector<int>(n+1, numeric_limits<int>::max())); // stop, node
-        
+
         for (int i = 0; i <= n; ++i)
             if (edge[src][i] != -1)
                 pq.push({0, i, edge[src][i]});
-        
+
         while (!pq.empty()) {
-            int stop, node, cur_price;
-            tie(stop, node, cur_price) = pq.top();
+            auto [stop, node, cur_price] = pq.top();
             pq.pop();
-            
+
             if (stop > K)
                 continue;
-            if (price[stop][node] < cur_price)
-                continue;
-            price[stop][node] = cur_price;
-            
+
+            if (node == dst)
+                return cur_price;
+
             for (int i = 0; i <= n; ++i) {
                 if (edge[node][i] != -1)
                     pq.push({stop+1, i, cur_price + edge[node][i]});
             }
         }
-            
-        int mn = numeric_limits<int>::max();
-        for (int i = 0; i <= K; ++i) {
-            mn = min(mn, price[i][dst]);
-        }
-        
-        return mn == numeric_limits<int>::max() ? -1 : mn;
+
+        return -1;
     }
 };
