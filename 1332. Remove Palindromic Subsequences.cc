211a8f3a23ad770a5e17da1bf1569dd5983b013a
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-08 22:01:31 +0800

    1332. Remove Palindromic Subsequences
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

---
+++ 1332. Remove Palindromic Subsequences.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if (s.empty())
+            return 0;
+
+        for (int i = 0; i <= s.size() / 2; ++i) {
+            if (s[i] != s[s.size() - i - 1])
+                return 2;
+        }
+
+        return 1;      
+    }
+};
