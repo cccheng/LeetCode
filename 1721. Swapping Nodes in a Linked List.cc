ecae6173b894e034276b16f94f7f15c90a45dcb1
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-14 21:13:41 +0800

    1721. v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 1721. Swapping Nodes in a Linked List.cc
+++ 1721. Swapping Nodes in a Linked List.cc
@@ -25,3 +25,39 @@ public:
         return new_head;
     }
 };
+
+// v2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *front, *back = head, *p = head;
+
+        int n = 0;
+        for (ListNode *node = head; node; node = node->next)
+            n++;
+        //if (k > n/2)
+        //    k = n - k;
+
+        while (--k)
+            p = p->next;
+        front = p;
+
+        while (p->next) {
+            p = p->next;
+            back = back->next;
+        }
+
+        swap(front->val, back->val);
+        return head;
+    }
+};
