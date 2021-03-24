83813210a86acbf84fb6df3153a01b354be76f76
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-24 22:44:39 +0800

    Gas Station
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

---
+++ 0134. Gas Station.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int tank, cur;;
+        int nr = gas.size();
+        for (int i = 0, s = 0; i < nr; ++i) {
+            tank = 0;
+            for (s = 0; s < nr; s++) {
+                cur = (i + s) % nr;
+                tank += gas[cur];
+                tank -= cost[cur];
+                if (tank < 0)
+                    break;
+            }
+
+            if (s == nr)
+                return i;
+        }
+
+        return -1;
+    }
+};
