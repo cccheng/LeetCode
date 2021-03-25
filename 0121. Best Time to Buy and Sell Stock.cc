39e6c4a5ce84ccff5fe19c19d8cc4d15246a17ce
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-03-25 19:14:40 +0800

    0121. DP
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0121. Best Time to Buy and Sell Stock.cc
+++ 0121. Best Time to Buy and Sell Stock.cc
@@ -13,3 +13,27 @@ public:
         return result;
     }
 };
+
+// v2. DP. similar to 0122
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mx = 0;
+
+        if (prices.empty())
+            return mx;
+
+        int buy = -prices[0];
+        int sold = 0;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            int _buy = max(buy, -prices[i]);
+            int _sold = max(sold, buy + prices[i]);
+            buy = _buy;
+            sold = _sold;
+            mx = max({mx, buy, sold});
+        }
+
+        return mx;
+    }
+};
