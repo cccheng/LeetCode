df1539c6e60401db01ce7b2914968a88c8aa2090
Chung-Chiang Cheng <shepjeng@gmail.com>
2021-04-05 21:54:11 +0800

    0376 v2
    Signed-off-by: Chung-Chiang Cheng <shepjeng@gmail.com>

--- 0376. Wiggle Subsequence.cc
+++ 0376. Wiggle Subsequence.cc
@@ -26,3 +26,34 @@ public:
         return mx;
     }
 };
+
+// v2
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int nr = nums.size();
+        int mx = 1;
+        vector<int> inc(nr);
+        vector<int> dec(nr);
+
+        if (nums.empty())
+            return 0;
+
+        inc[0] = 1;
+        dec[0] = 1;
+        for (int i = 1; i < nr; ++i) {
+            if (nums[i] > nums[i-1]) {
+                inc[i] = max(dec[i-1]+1, inc[i-1]);
+                dec[i] = dec[i-1];
+            } else if (nums[i] < nums[i-1]) {
+                inc[i] = inc[i-1];
+                dec[i] = max(inc[i-1]+1, dec[i-1]);
+            } else {
+                inc[i] = inc[i-1];
+                dec[i] = dec[i-1];
+            }
+            mx = max({mx, inc[i], dec[i]});
+        }
+        return mx;
+    }
+};
